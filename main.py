9b83dfc1a920799eb806030540068ee6418c7d28
segomezz <141938389+segomezz@users.noreply.github.com>
2023-08-20 20:27:51 -0500

    main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-Class Asiento:
+class Asiento:
     def __init__(self,color,precio,registro):
         self.color=color
         self.precio=precio 
@@ -7,7 +7,7 @@ Class Asiento:
             if colorn in ["amarillo", "verde", "blanco", "negro", "rojo"]:
                 self.color = colorn
 
-Class Motor:
+class Motor:
     def __init__(self,numeroCilindros,tipo,registro):
         self.numeroCilindros=numeroCilindros
         self.tipo=tipo
@@ -17,7 +17,7 @@ Class Motor:
         def asignarTipo(self,tipon):
             if tipon in ["gasolina", "electrico"]:
                 self.tipo=tipon
-Class Auto:
+class Auto:
     def __init__(self,modelo,precio,motor,marca,asientos=None,registro=None,cantidadCreados=0):
         self.modelo=modelo
         self.registro=registro
