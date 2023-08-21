32de2f2d94308f87377115e27a23455f24dc25ee
segomezz <141938389+segomezz@users.noreply.github.com>
2023-08-20 20:31:51 -0500

    main

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ class Asiento:
         self.color=color
         self.precio=precio 
         self.registro=registro
-        def cambiarColor(self,colorn):
-            if colorn in ["amarillo", "verde", "blanco", "negro", "rojo"]:
-                self.color = colorn
+    def cambiarColor(self,colorn):
+        if colorn in ["amarillo", "verde", "blanco", "negro", "rojo"]:
+                elf.color = colorn
 
 class Motor:
     def __init__(self,numeroCilindros,tipo,registro):
         self.numeroCilindros=numeroCilindros
         self.tipo=tipo
         self.registro=registro
-        def cambiarRegistro (self,registron):
-            self.registro=registron
-        def asignarTipo(self,tipon):
-            if tipon in ["gasolina", "electrico"]:
-                self.tipo=tipon
+    def cambiarRegistro (self,registron):
+        self.registro=registron
+    def asignarTipo(self,tipon):
+        if tipon in ["gasolina", "electrico"]:
+            self.tipo=tipon
 class Auto:
     def __init__(self,modelo,precio,motor,marca,asientos=None,registro=None,cantidadCreados=0):
         self.modelo=modelo
@@ -27,18 +27,18 @@ class Auto:
         self.asientos=asientos if asientos is not None else[]
         Auto.cantidadCreados=cantidadCreados
        
-        def cantidadAsientos(self):
-            num_asientos = 0
-            for asiento in self.asientos:
-                if asiento is not None:
-                    num_asientos += 1
-            return num_asientos
+    def cantidadAsientos(self):
+        num_asientos = 0
+        for asiento in self.asientos:
+            if asiento is not None:
+                num_asientos += 1
+        return num_asientos
     
-        def verificarIntegridad(self):
-            if self.registro == self.Motor.registro:
-                for asiento in self.asientos:
-                    if asiento is not None and asiento.registro != self.registro:
-                        return "Las piezas no son originales"
-                return "Auto original"
-            else:
-                return "Las piezas no son originales"
+    def verificarIntegridad(self):
+        if self.registro == self.Motor.registro:
+            for asiento in self.asientos:
+                if asiento is not None and asiento.registro != self.registro:
+                    return "Las piezas no son originales"
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
